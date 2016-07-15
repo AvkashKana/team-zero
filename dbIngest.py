241a1163f4f42f5f726a4c0bafa340b90a1f9603
Devin Brande <dbrande@gmail.com>
2016-07-15 18:03:28 -0400

    First working version

--- dbIngest.py
+++ dbIngest.py
@@ -1,7 +1,17 @@
 import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
-engine = create_engine('postgresql://Devin:b7410b@localhost:5432/reviews')
+
+engine = create_engine('postgresql+psycopg2://teamzero:123456@localhost:5432/teamzeroDB')
 
 df = pd.read_csv('/Users/Devin/Repos/Georgetown/TZ/reviews 2.csv')
-df.to_sql("reviews", engine)
+df.to_sql("reviews", engine, if_exists='replace')
+
+df = pd.read_csv('/Users/Devin/Repos/Georgetown/TZ/neighbourhoods.csv')
+df.to_sql("neighborhoods", engine, if_exists='replace')
+
+df = pd.read_csv('/Users/Devin/Repos/Georgetown/TZ/listings.csv')
+df.to_sql("listings", engine, if_exists='replace')
+
+df = pd.read_csv('/Users/Devin/Repos/Georgetown/TZ/calendar.csv')
+df.to_sql("calendar", engine, if_exists='replace')
