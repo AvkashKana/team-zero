dfe1ae1f4131580c3dddc94d22d5fd448a18d185
Devin Brande <dbrande@gmail.com>
2016-07-26 00:24:49 -0400

    "Added comments for clarity"

--- dbIngest.py
+++ dbIngest.py
@@ -1,8 +1,28 @@
+# usr/bin/evn python2.7
+#
+# dbIngest.py -- This script will create several PostgreSQL
+# tables from the baseline Insideairbnb.com baseline CSV files
+
+
 import pandas as pd
 from sqlalchemy import create_engine
 import psycopg2
 
+# Creating a sqlalchemy engine connected to the local database,
+# 'teamzeroDB'.
+# The database and login role must already exist before running
+# the script.
+
+# In this case I'm using the login role 'teamzero' with
+# a throwaway password, '123456'. My PostgreSQL server is listening
+# on the default port, 5432.
+
 engine = create_engine('postgresql+psycopg2://teamzero:123456@localhost:5432/teamzeroDB')
+
+# Read the CSV files and then write them to a new table in the db.
+
+# WARNING!! This is set to replace the table if it already exists,
+# This code might wreck your existing tables.
 
 df = pd.read_csv('/Users/Devin/Repos/Georgetown/TZ/reviews 2.csv')
 df.to_sql("reviews", engine, if_exists='replace')
