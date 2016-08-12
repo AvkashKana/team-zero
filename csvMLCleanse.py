81d3aeb282430b8053c3ca93113921beb1e87793
Devin Brande <dbrande@gmail.com>
2016-08-11 22:47:37 -0400

    Final CSV cleaning code

--- csvMLCleanse.py
+++ csvMLCleanse.py
@@ -8,6 +8,7 @@
 
 import csv
 import re
+from itertools import izip
 
 listings = open('listings.csv')
 listingReader = csv.reader(listings)
@@ -135,10 +136,164 @@ state = state
 # with 00000 (Nowheresville, USA)
 zip_code = [i[:5] for i in zip_code]
 zip_code = [(int(i) if i else '00000') for i in zip_code]
-print zip_code
+zip_code = [int(i) for i in zip_code]
+# Latitude gets converted to a float
+lat = [float(i) for i in lat]
+# Longitude gets converted to a float
+longitude = [float(i) for i in longitude]
+# Property type can remain as a string
+prop_type = prop_type
+# Room type can remain as a string
+room_type = room_type
+# Accommodates gets converted to an int
+accom = [int(i) for i in accom]
+# Bath gets converted to an int, empty cells replaced with 0
+bath = [(float(i) if i else '0') for i in bath]
+bath = [float(i) for i in bath]
+# Bedrooms gets converted to an int. Empty cells replaced
+# with 0
+bedrooms = [(int(i) if i else '0') for i in bedrooms]
+bedrooms = [int(i) for i in bedrooms]
+# Beds gets converted to an int, empty cells replaced with 0
+beds = [(int(i) if i else '0') for i in beds]
+beds = [int(i) for i in beds]
+# Square feet gets converted to a float, empty cells are
+# replaced with 0.  However, because there are only 82
+# values total and only 74 non-zero values, the 0 replacements
+# should likely be excluded and/or adjusted.
+sq_ft = [(float(i) if i else '0') for i in sq_ft]
+sq_ft = [float(i) for i in sq_ft]
+# Price gets covnerted to a float. First, remove the '$'
+# symbol, then remove the ',' marks, then convert to float
+price = [i.lstrip('$') for i in price]
+price = [i.replace(',','') for i in price]
+price = [float(i) for i in price]
+# Weekly price gets the same treatment as price. Empty cells
+# are replaced with '0'
+week_price = [i.lstrip('$') for i in week_price]
+week_price = [i.replace(',','') for i in week_price]
+week_price = [(float(i) if i else '0') for i in week_price]
+week_price = [float(i) for i in week_price]
+# Monthly price gets the same treatment as weekly price
+month_price = [i.lstrip('$') for i in month_price]
+month_price = [i.replace(',','') for i in month_price]
+month_price = [(float(i) if i else '0') for i in month_price]
+month_price = [float(i) for i in month_price]
+# Security deposit gets the same as monthly and weekly price
+sec_deposit = [i.lstrip('$') for i in sec_deposit]
+sec_deposit = [i.replace(',','') for i in sec_deposit]
+sec_deposit = [(float(i) if i else '0') for i in sec_deposit]
+sec_deposit = [float(i) for i in sec_deposit]
+# Cleaning fee gets the same treatment as security deposit,
+# monthly price, and weekly price.
+cleaning_fee = [i.lstrip('$') for i in cleaning_fee]
+cleaning_fee = [i.replace(',','') for i in cleaning_fee]
+cleaning_fee = [(float(i) if i else '0') for i in cleaning_fee]
+cleaning_fee = [float(i) for i in cleaning_fee]
+# Guests included gets converted to an int
+guest_inc = [int(i) for i in guest_inc]
+# Extra people gets the same treatment as price
+extra_peop = [i.lstrip('$') for i in extra_peop]
+extra_peop = [i.replace(',','') for i in extra_peop]
+extra_peop = [float(i) for i in extra_peop]
+# Minimum nights gets converted to an int
+min_night = [int(i) for i in min_night]
+# Maximum nights gets converted to an int
+max_night = [int(i) for i in max_night]
+# 30 day availability gets converted to an int
+avail_30 = [int(i) for i in avail_30]
+# 60 day availability gets converted to an int
+avail_60 = [int(i) for i in avail_60]
+# 90 day availability gets converted to an int
+avail_90 = [int(i) for i in avail_90]
+# 365 day availability gets covnerted to an int
+avail_365 = [int(i) for i in avail_365]
+# Number of reviews gets converted to an int
+number_reviews = [int(i) for i in number_reviews]
+# First review stays as a string.
+f_review = f_review
+# Last review stays as a string.
+l_review = l_review
+# Review scores rating gets convert to an int.
+# Empty cells are replaced with 0. They should
+# probably be ignored in any calculations.
+review_score = [(int(i) if i else '0') for i in review_score]
+review_score = [int(i) for i in review_score]
+# Review scores accuracy gets the same treatment
+# as review scores review scores rating
+review_score_acc = [(int(i) if i else '0') for i in review_score_acc]
+review_score_acc = [int(i) for i in review_score_acc]
+# Review scores cleanliness gets the same treatment
+# as review scores rating and accuracy
+review_score_clean = [(int(i) if i else '0') for i in review_score_clean]
+review_score_clean = [int(i) for i in review_score_clean]
+# Review scores checkin gets the same treatment as the others
+review_score_checkin = [(int(i) if i else '0') for i in review_score_checkin]
+review_score_checkin = [int(i) for i in review_score_checkin]
+# Review scores communication gets the same treatment as others
+review_score_comm = [(int(i) if i else '0') for i in review_score_comm]
+review_score_comm = [int(i) for i in review_score_comm]
+# Review scores location gets the same treatment as others
+review_score_loc = [(int(i) if i else '0') for i in review_score_loc]
+review_score_loc = [int(i) for i in review_score_loc]
+# Review scores value gets the same treatment as others
+review_score_value = [(int(i) if i else '0') for i in review_score_value]
+review_score_value = [int(i) for i in review_score_value]
+# Reviews per month gets converted to a float. Empty cells are
+# replaced with '0'
+review_permonth = [(float(i) if i else '0') for i in review_permonth]
+review_permonth = [float(i) for i in review_permonth]
 
-# print zip_code[1]
-# print type(zip_code[1])
+print review_permonth[1]
+print type(review_permonth[1])
+
+# Write all these rows to a CSV
+with open('listings_clean.csv', 'wb') as f:
+    writer = csv.writer(f)
+    writer.writerows(izip(l_ID,
+    host_ID,
+    host_name,
+    host_since,
+    host_RR,
+    host_AR,
+    host_LC,
+    neigh_C,
+    state,
+    zip_code,
+    lat,
+    longitude,
+    prop_type,
+    room_type,
+    accom,
+    bath,
+    bedrooms,
+    beds,
+    sq_ft,
+    price,
+    week_price,
+    month_price,
+    sec_deposit,
+    cleaning_fee,
+    guest_inc,
+    extra_peop,
+    min_night,
+    max_night,
+    avail_30,
+    avail_60,
+    avail_90,
+    avail_365,
+    number_reviews,
+    f_review,
+    l_review,
+    review_score,
+    review_score_acc,
+    review_score_clean,
+    review_score_checkin,
+    review_score_comm,
+    review_score_loc,
+    review_score_value,
+    review_permonth,
+    ))
 
 # print('The median accommodation number is ' + str(statistics.median(listingAccom)))
 #
